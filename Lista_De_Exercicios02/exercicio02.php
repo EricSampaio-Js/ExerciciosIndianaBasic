4f77d0f385a5faf2676020d04d911089a8460a86
Eric <eric.arena7@gmail.com>
2023-08-06 21:25:38 -0300

    adicionando algumas alterações

--- Lista_De_Exercicios02/exercicio02.php
+++ Lista_De_Exercicios02/exercicio02.php
@@ -5,12 +5,15 @@ entre 0 e 10. Encontre e mostre o menor elemento e sua posição no vetor. -->
 <?php
 
 $num =  array_fill(0, 5, null);
-
 foreach ($num as $indice => $valor){
-    $num[$indice] = rand(0, 10);
-    echo $num[$indice];
+    $num[$indice] = mt_rand(0, 10);
 }
 
+$menorValor = min($num);
+$posicao = array_search($menorValor,$num);
+
+echo "o Menor valor e $menorValor e a sua posicao é $posicao";
+ 
 echo'<pre>';
 print_r($num);
 echo'</pre>';
