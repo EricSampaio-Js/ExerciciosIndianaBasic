fa519b52edde3b286fc0761cac19c49ad4eeebcf
Eric <eric.arena7@gmail.com>
2023-08-13 23:39:10 -0300

    Finalizando a Classe Person

--- Poo_exercicio/person.php
+++ Poo_exercicio/person.php
@@ -1,74 +1,201 @@
-<?php
+<!-- A classe Pessoa deve ter os seguintes atributos:
+- nome;
+- idade;
+- corDosOlhos;
+- gênero;
+- altura;
+- peso;
+
+No caso do atributo corDosOlhos só poderá ser inserido uma dessas opções: 
+- Azul;
+- Castanho;
+- Verde;
+- Preto.
+
+E no gênero estas opções:
+- Masculino;
+- Feminino;
+
+Tarefa: Implemente o encapsulamento na sua classe Pessoa levando em consideração as informações acima. -->
+
+<?php 
+
 class Person {
-    //Atributos
-    public  string $corOlhos;
-    public int $qtdBraco;
-    public int $qtdPena;
 
-    public bool $correr;
-    public bool $andar;
+  public function __construct(
+    private string $nome,
+    private string $corDosOlhos,
+    private string $genero,
 
+    private int $altura,
+    private int $peso,
+    private int $idade
+  ){
 
-    ################# METODOS #################################
+    $verifiqueCorDosOlhos = 
+    strtolower($this->corDosOlhos) == strtolower('Azul')  || 
+    strtolower($this->corDosOlhos) == strtolower('Castanho')  || 
+    strtolower($this->corDosOlhos) == strtolower('Verde')  || 
+    strtolower($this->corDosOlhos) == strtolower('Preto'); 
+
+    if($verifiqueCorDosOlhos){
+      $this->corDosOlhos = ucfirst($corDosOlhos) ; 
+  }else{
+      echo 
+      "
+      Error : Cor não permitida <br>
+      Use Apenas <br>
+          Azul
+          Castanho
+          Verde
+          Preto
+      ";
+    }
+
+    $verifiqueGenero =  
+    strtolower($genero) == strtolower('Feminino') ||
+    strtolower($genero) == strtolower('Masculino') ;
     
-    ################  COR OLHOS #################################
-    
-    public function setCorOlhos(string $corOlhos): void
-    {
-        $this->corOlhos = $corOlhos;
+    if($verifiqueGenero){
+      $this->genero = ucfirst($genero);
+  }else{
+      echo 
+      "
+      Error : Genero não permitido <br>
+      Use Apenas <br>
+          Masculino ou   Feminino 
+      ";
     }
 
-    public function getCorOlhos(): string
-    {
-        return $this->corOlhos;
-    }
-    ############ BRAÇO ###############################
-    public function setqQtdBraco(int $qtdBraco): void
-    {
-        $this->qtdBraco = $qtdBraco;
-    }
+  }
 
-    public function getQtdBraco(): int
-    {
-        return $this->qtdBraco;
-    }
+    ##################### NOME ###############
+    public function setNome (string $nome):void 
+    {   $this->nome = $nome;    }
 
-    ############ PERNAS ##############################
+    public function getNome(): string
+    {   return $this->nome;   }
 
-   public function setQtdPena(int $qtdPena): void
-    {
-        $this->qtdPena = $qtdPena;
-    }
+    ##################### OLHOS ###############
+    // - Azul;
+    // - Castanho;
+    // - Verde;
+    // - Preto.
 
-    public function getQtdPena(): int
-    {
-        return $this->qtdPena;
-    }
-    ##################### AÇÕES ##############################
   
-
-    public function setCorrer(bool $correr): void
-    {
-        $this->correr = $correr;
+    public function setCorDosOlhos (string $corDosOlhos):void 
+    {   
+      $verifiqueCorDosOlhos = 
+      strtolower($corDosOlhos) == strtolower('Azul')  || 
+      strtolower($corDosOlhos) == strtolower('Castanho')  || 
+      strtolower($corDosOlhos) == strtolower('Verde')  || 
+      strtolower($corDosOlhos) == strtolower('Preto'); 
+        
+        if($verifiqueCorDosOlhos){
+            $this->corDosOlhos = ucfirst($corDosOlhos) ; 
+        }else{
+            echo 
+            "
+            Error : Cor não permitida <br>
+            Use Apenas <br>
+                Azul
+                Castanho
+                Verde
+                Preto
+            ";
+            
+        }
     }
+    
+    public function getCorDosOlhos(): string
+    {   return $this->corDosOlhos;  }
 
-    public function getCorrer(): string
-    {
-         return  $this->correr;
+  
+    ################### GENERO ##################
+    // - Masculino;
+    // - Feminino;
+    public function setGenero (string $genero):void 
+    {   
+
+        $vefiqGenero = 
+        strtolower($genero) == strtolower('Feminino') ||
+        strtolower($genero) == strtolower('Masculino') ;
+        
+        if($vefiqGenero){
+            $this->genero = ucfirst($genero);
+        } else {
+            echo 
+            "
+            Error : Genero não permitido <br>
+            Use Apenas <br>
+                Masculino <br> 
+                Feminino 
+            ";
+        }
     }
+    
+    public function getGenero(): string
+    {   return $this->genero;  }
 
-    ############## ANDAR    #############################
+    ################### ALTURA ##################
+    public function setAltura (int $altura):void 
+      {   $this->genero = $altura; }
+      
+    public function getAltura(): int
+      {   return $this->altura;  }
 
-    public function setAndar(bool $andar): void
-    {
-        $this->andar = $andar;
-    }
+    ################### PESO ##################
+    public function setPeso (int $peso):void 
+      {   $this->peso = $peso; }
+      
+    public function getPeso(): int
+      {   return $this->peso;  }
 
-    public function getAndar(): string
-    {
-        return $this->andar;
-    }
-
+    ################### IDADE ##################
+    public function setIdade (int $idade):void 
+      {   $this->idade = $idade; }
+      
+    public function getIdade(): int
+      {   return $this->idade;  }
 
 }
+
+// private string $nome,
+// private string $corDosOlhos,
+// private string $genero,
+
+// private int $altura,
+// private int $peso,
+// private int $idade
+
+$objetoPerson1 = new Person('joão','Verde','Masculino',12,1.5,50);
+
+// lançando ERROR
+// $objetoPerson1 = new Person('joão','Verde','Mascasdaulino',12,1.5,50);
+
+echo "<br>";
+
+echo $objetoPerson1->getNome();  
+echo "<br>";
+echo $objetoPerson1->getCorDosOlhos();
+echo "<br>";
+
+// lançando Error 
+// echo $objetoPerson1->setCorDosOlhos('sdadasdda');
+
+echo "<br>";
+echo $objetoPerson1->getGenero();
+echo "<br>";
+
+// Lançando Error
+// echo $objetoPerson1->setGenero('femindsadasino');
+echo "<br>";
+echo $objetoPerson1->getGenero();
+echo "<br>";
+
+echo $objetoPerson1->getIdade();
+
+
+
+
 ?>
